cf265d06be998916cb7cc340ee0ffc6b5c402f83
RiDevOps <68944298+RiDevOps@users.noreply.github.com>
2026-02-17 17:19:12 +0530

    Add outputs file

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.web.id
+}
